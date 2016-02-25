1a27bc078f5866738fd00f0133d533c02150fda8
spzktshow <gdspzkt@gmail.com>
2016-02-26 00:36:31 +0800

    cmi

--- OriginArtPro/Classes/originGame/base/OGState.h
+++ OriginArtPro/Classes/originGame/base/OGState.h
@@ -5,13 +5,13 @@
 
 NS_OG_BEGIN
 
-class StateComponentDefiniation : public origin::DynamicStateDefiniation
+class StateFactorDefiniation : public origin::DynamicStateDefiniation
 {
 public:
 	static const float TIME_DATA_MAX;
 
-	StateComponentDefiniation(const std::string& type, const origin::ExclusionDefiniation * exclusionDefiniation);
-	virtual ~StateComponentDefiniation();
+	StateFactorDefiniation(const std::string& type, const origin::ExclusionDefiniation * exclusionDefiniation);
+	virtual ~StateFactorDefiniation();
 
 	/**持续时间，这个时间是标准时间，表示正常速度下人物播放帧的时间，如果人物有加速状态则时间会变短,
 	如果值为<=0则表示这个状态为循环状态,time的单位为秒，精确到小数点后一位
@@ -19,13 +19,13 @@ public:
 	CC_SYNTHESIZE(float, _time, Time);
 };
 
-class StateComponent : public origin::DynamicState
+class StateFactor : public origin::DynamicState
 {
 public:
-	StateComponent(const StateComponentDefiniation * stateComponentDefiniation);
-	virtual ~StateComponent();
+	StateFactor(const StateFactorDefiniation * stateComponentDefiniation);
+	virtual ~StateFactor();
 
-	const StateComponentDefiniation * getStateComponentDefiniation() const;
+	const StateFactorDefiniation * getStateComponentDefiniation() const;
 
 	void startExecute() override;
 	void stopExecute() override;
--- OriginArtPro/Classes/originGame/unit/OGUnitStateBehaviorTree.h
+++ OriginArtPro/Classes/originGame/unit/OGUnitStateBehaviorTree.h
@@ -3,12 +3,39 @@
 
 #include "OriginGameMacros.h"
 #include "OGUnitState.h"
+#include "OBehaviorTree.h"
 
 NS_OG_BEGIN
 
 //=========================Condition===============================
+/***********当前状态等于*********/
+class UnitCurrentStateEqualCondition : public ConditionBehaviorTreeNode
+{
+public:
+	static const std::string TYPE;
 
+	UnitCurrentStateEqualCondition(const std::string& currentState);
+	~UnitCurrentStateEqualCondition();
 
+	bool execute(cocos2d::EventCustom * event) override;
+};
+
+/*********当前状态生命周期比较*******/
+class UnitCurrentStateLifeTimeEqualCondition : public ConditionBehaviorTreeNode
+{
+public:
+	static const std::string TYPE;
+	enum class EqualSign
+	{
+		LESS = -1,
+		EQUAL = 0,
+		GREATER = 1,
+	};
+	UnitCurrentStateLifeTimeEqualCondition(const EqualSign&equalSign);
+	~UnitCurrentStateLifeTimeEqualCondition();
+
+	bool execute(cocos2d::EventCustom * event) override;
+};
 //=========================Action==================================
 NS_OG_END
 
